5c281ab25ff6f69d032ccd6937a95f490d10b6a6
SofiaSagar <107825794+SofiaSagar@users.noreply.github.com>
2022-07-24 20:26:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+
+st.write("""
+# DIVISION OF TWO NUMBERS
+""")
+#Get Input
+
+st.header('GIVE US SOME NUMBERS FOR FUN!')
+
+def user_input_features():
+
+    NUM1 = st.number_input("NUMERATOR")       #,min_value=0,max_value=20,step=1)
+    NUM2 = st.number_input("DENOMINATOR",value=1) 
+                                               #,min_value=0,max_value=20,step=1)
+    if NUM2==0:
+        st.write("OH!BY THE WAY DENOMINATOR CANT BE ZERO")
+    
+    result=NUM1/NUM2
+    return result
+
+df = user_input_features()    
+
+st.write("THE RESULT OF YOUR DIVISION IS : ")
+st.write(df)
